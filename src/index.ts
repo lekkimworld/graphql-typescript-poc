fde8d0201f0c9ba862c8932c180495760972ba76
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-04-14 15:33:04 +0200

    Custom type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import "reflect-metadata";
 import {ApolloServer} from "apollo-server-express";
 import * as Express from "express";
-import {buildSchema, Resolver, Query} from "type-graphql";
+import {buildSchema, Resolver, Query, Field, ObjectType, ID, FieldResolver, Root, Arg } from "type-graphql";
+
+@ObjectType()
+class Greeting {
+    @Field(() => ID, {nullable: false})
+    readonly value : string;
+    constructor(value : string) {
+        this.value = value;
+    }
+    @Field()
+    readonly sentence : string;
+}
+
+const greetings = new Map<string, Greeting>();
+greetings.set("da", new Greeting("Hej"));
+greetings.set("fr", new Greeting("Bonjour"));
+greetings.set("en", new Greeting("Hello"));
+/* 
+class GetGreetingArgs {
+    @Field()
+    lang : string;
+}
+*/
 
 @Resolver()
 class HelloResolver {
@@ -11,18 +33,31 @@ class HelloResolver {
     }
 }
 
+@Resolver(Greeting)
+class GreetingResolver {
+    @FieldResolver()
+    async sentence(@Root() parent : Greeting) {
+        return `${parent.value} World!`;
+    }
+
+    @Query(() => Greeting)
+    async greeting(@Arg("lang") lang : string) {
+        return greetings.get(lang);
+    }
+}
+
 const main = async () => {
     const schema = await buildSchema({
-        resolvers: [HelloResolver]
+        resolvers: [HelloResolver, GreetingResolver]
     })
 
-    if (process.env.GRAPHQL_ENABLE_PLAYGROUND !== undefined) {
+    if (process.env.NODE_ENV === undefined || process.env.GRAPHQL_ENABLE_PLAYGROUND !== undefined) {
         console.log("Enabling GraphQL Playground");
     }
     const apolloServer = new ApolloServer({ 
         schema, 
-        introspection: process.env.GRAPHQL_ENABLE_PLAYGROUND !== undefined,
-        playground: process.env.GRAPHQL_ENABLE_PLAYGROUND !== undefined 
+        introspection: process.env.NODE_ENV === undefined || process.env.GRAPHQL_ENABLE_PLAYGROUND !== undefined,
+        playground: process.env.NODE_ENV === undefined || process.env.GRAPHQL_ENABLE_PLAYGROUND !== undefined 
     });
     const app = Express();
     apolloServer.applyMiddleware({
