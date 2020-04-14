170156337ff3059e74dc4b1a6815cb4404b84e50
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-04-14 14:36:17 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import {ApolloServer} from "apollo-server-express";
+import * as Express from "express";
+import {buildSchema, Resolver, Query} from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+    @Query(() => String, {name: "hellolekkim", description: "Says hello to lekkim", nullable: false})
+    async hello() {
+        return "Hello, lekkim, World!!!";
+    }
+}
+
+const main = async () => {
+    const schema = await buildSchema({
+        resolvers: [HelloResolver]
+    })
+
+    const apolloServer = new ApolloServer({ schema });
+    const app = Express();
+    apolloServer.applyMiddleware({
+        "app": app
+    });
+
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Server now listening on http://localhost:${process.env.PORT || 4000}/graphql`);
+    })
+}
+
+main();
